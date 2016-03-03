ea683d6571f719fee3e49623fa4df5369cd6e45e
mykolakondratyuk <mykolakondratyuk@gmail.com>
2016-03-03 20:04:28 +0300

    copy to github

---
+++ MyTestFastenApp/MyTestFastenApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestFastenApp
+//
+//  Created by Developer on 3/3/16.
+//  Copyright © 2016 M. Kondratyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestFastenApp/MyTestFastenApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastenTestApp
+//
+//  Created by Developer on 3/2/16.
+//  Copyright © 2016 M. Kondratyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
